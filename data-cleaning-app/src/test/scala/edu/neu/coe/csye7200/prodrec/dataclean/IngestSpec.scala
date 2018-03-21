f19043c8fcca77a04fe03c28ccfbdab932ee9519
Vaishali Lambe <lambe.v@husky.neu.edu>
2018-03-21 17:26:11 +0000

    IngestSpec.Scala first Unit test case to verify that record from input .csv file is read

--- data-cleaning-app/src/test/scala/edu/neu/coe/csye7200/prodrec/dataclean/IngestSpec.scala
+++ data-cleaning-app/src/test/scala/edu/neu/coe/csye7200/prodrec/dataclean/IngestSpec.scala
@@ -6,3 +6,24 @@ import org.scalatest.{FlatSpec, Matchers}
 class IngestSpec extends FlatSpec with Matchers {
 
 }
+
+import org.scalatest.FunSuite
+import org.scalatest.Matchers._
+
+/**
+  *
+  * @author Luciano Molinari
+  */
+class CustCSVReaderTest extends FunSuite {
+
+  test("Load CSV file") {
+    val customers = new CustCSVReader("src/main/resources/train_ver2.csv").readCust
+
+    //customers.size shouldBe 0
+
+    customers(0) shouldBe Customer("2015-01-28","1375586","N","ES","H", "35","2015-01-12", "0","6", "1","","1.0","A","S","N","","KHL","N", "1","29","MALAGA", "1",
+      "87218.1","02 - PARTICULARES")
+
+  }
+
+}
