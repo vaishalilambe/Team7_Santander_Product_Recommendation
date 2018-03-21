dee167b2f21cabb488d53d9c228506674ed1664f
Vaishali Lambe <lambe.v@husky.neu.edu>
2018-03-21 17:35:35 +0000

    WIP #4 : First unit test case to read data from input .csv file

--- data-cleaning-app/src/test/scala/edu/neu/coe/csye7200/prodrec/dataclean/IngestSpec.scala
+++ data-cleaning-app/src/test/scala/edu/neu/coe/csye7200/prodrec/dataclean/IngestSpec.scala
@@ -1,12 +1,5 @@
 package edu.neu.coe.csye7200.prodrec.dataclean
 
-import org.scalatest.{FlatSpec, Matchers}
-
-
-class IngestSpec extends FlatSpec with Matchers {
-
-}
-
 import org.scalatest.FunSuite
 import org.scalatest.Matchers._
 
