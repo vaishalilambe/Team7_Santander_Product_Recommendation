f4ec8223ec496a3923142151de3b9398b98f8d01
Nishant <nishantgandhi99@gmail.com>
2018-03-27 19:49:45 -0400

    Fixed #27 Remove dataframe support

--- data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/main/AppRunner.scala
+++ data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/main/AppRunner.scala
@@ -23,34 +23,5 @@ object AppRunner extends Serializable {
       case None =>
     }
 
-//    val filePath = "data-cleaning-app/src/main/resources/train_ver2.csv"
-//
-//    val spark = SparkSession
-//      .builder
-//      .master("local")
-//      .appName("DataCleaning")
-//      .getOrCreate()
-//
-//    var df = spark.read
-//      .option("header", "true")
-//      //    .schema(customSchema)
-//      .csv(filePath)
-//
-//    //  df.printSchema()
-//
-//    df = DFUtils.removeWhitespaceAllColumns(df)
-//
-//    df = df.filter(not(col("age") === ""))
-//
-//    df = DFUtils.castColumnTo(df, "age", IntegerType)
-//
-//    df = df.filter(col("age") > 27)
-//
-//    df.write.format("csv").save("destination")
-//
-//    df.show()
-//
-//    spark.close()
-
   }
 }
