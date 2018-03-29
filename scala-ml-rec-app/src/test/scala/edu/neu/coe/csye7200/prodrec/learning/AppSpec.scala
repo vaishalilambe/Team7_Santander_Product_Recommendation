0c596dbad2d9dd5dca5c4a4ab6567f411f5bb8c9
arpitHub <rawat.a@husky.neu.edu>
2018-03-28 22:32:50 -0400

    WIP #8 added test case for data model app

--- scala-ml-rec-app/src/test/scala/edu/neu/coe/csye7200/prodrec/learning/AppSpec.scala
+++ scala-ml-rec-app/src/test/scala/edu/neu/coe/csye7200/prodrec/learning/AppSpec.scala
@@ -1,1 +1,12 @@
 package edu.neu.coe.csye7200.prodrec.learning
+
+import org.scalatest.{FlatSpec, Matchers}
+
+class AppSpec extends FlatSpec with Matchers {
+
+  behavior of "app"
+
+  it should "example test for ml" in {
+    assert(1 == 1)
+  }
+}
