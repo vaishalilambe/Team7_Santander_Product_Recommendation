ab61a68e16bc4d8ab8417d36fe8f1f9a7942d0a6
Nishant <nishantgandhi99@gmail.com>
2018-04-05 18:49:45 -0400

    WIP: #19 Data Cleaning and Preprocessing, improved App Runner

--- data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/main/AppRunner.scala
+++ data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/main/AppRunner.scala
@@ -1,5 +1,9 @@
 package edu.neu.coe.csye7200.prodrec.dataclean.main
 
+import edu.neu.coe.csye7200.prodrec.dataclean.io.DataParser
+import org.apache.spark.sql.SparkSession
+//import edu.neu.coe.csye7200.prodrec.dataclean.io
+
 case class Config(input: String = null, output:String = null)
 
 object AppRunner extends Serializable {
@@ -20,6 +24,23 @@ object AppRunner extends Serializable {
       case Some(config) =>
         val input = config.input
         val output = config.output
+
+        val ss = SparkSession
+          .builder()
+          .appName("Data Cleaning App")
+          .master("local")
+          .config("spark.debug.maxToStringFields",100)
+          .getOrCreate()
+
+        val stringDS = DataParser.getStringDS(input,ss)
+
+        print(stringDS.count())
+
+        val classDS = DataParser.stringDStoClassDS(stringDS, ss)
+
+        print(classDS.count())
+
+
       case None =>
     }
 
