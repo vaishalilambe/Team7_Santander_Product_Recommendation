3ca58fd9c46dc6d0596bad48a81109704253145d
Nishant <nishantgandhi99@gmail.com>
2018-04-06 00:25:27 -0400

    Fixed #4 Writing Unit Test Cases

---
+++ data-cleaning-app/src/test/scala/edu/neu/coe/csye7200/prodrec/dataclean/SantenderRecordSpec.scala
@@ -0,0 +1,63 @@
+package edu.neu.coe.csye7200.prodrec.dataclean
+
+import edu.neu.coe.csye7200.prodrec.dataclean.model.{Account, Customer, Product}
+import org.scalatest.{FlatSpec, Matchers}
+
+
+class SantenderRecordSpec extends FlatSpec with Matchers {
+
+  behavior of "Customer"
+
+  it should "work for input" in {
+    Customer("12345", "A", "India", "M", "34", "34000") should matchPattern {
+      case Customer(12345, "A", "India", "M", Some(34), Some(34000)) =>
+    }
+    Customer("34567", "B", "China", "F", "", "50000") should matchPattern {
+      case Customer(34567, "B", "China", "F", None, Some(50000)) =>
+    }
+    Customer("34567", "B", "China", "F", "", "") should matchPattern {
+      case Customer(34567, "B", "China", "F", None, None) =>
+    }
+  }
+
+  behavior of "Account"
+
+  it should "work for input" in {
+    val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+    val sqlDate = new java.sql.Date(format.parse("2015-11-12").getTime)
+
+    Account("01 - VIP", "2015-11-12", "1", "4", "1", "1", "A-Active", "S", "S", "channel", "N", "NA", "1") should matchPattern {
+      case Account("01 - VIP", sqlDate, Some(1), Some(4), Some(1), Some("1"), Some("A-Active"), Some("S"), Some("S"), Some("channel"), Some("N"), Some("NA"), Some(1)) =>
+    }
+    Account("01 - VIP", "2015-11", "1", "", "1", "1", "A-Active", "S", "S", "channel", "N", "NA", "2d1") should matchPattern {
+      case Account("01 - VIP", None, Some(1), None, Some(1), Some("1"), Some("A-Active"), Some("S"), Some("S"), Some("channel"), Some("N"), Some("NA"), None) =>
+    }
+    Account("01 - VIP", "201-12", "1d", "4", "", "1", "A-Active", "S", "S", "channel", "N", "NA", "1") should matchPattern {
+      case Account("01 - VIP", None, None, Some(4), None, Some("1"), Some("A-Active"), Some("S"), Some("S"), Some("channel"), Some("N"), Some("NA"), Some(1)) =>
+    }
+  }
+
+  behavior of "Product"
+
+  it should "work for input" in {
+    Product("0","1","0","1","1","0","1","0","1","1","1","1","0","1","0","1","1","0","1","0","1","1","1","1") should matchPattern{
+      case Product(false,true,false,true,true,false,true,false,true,true,true,true,false,true,false,true,true,false,true,false,true,true,true,true) =>
+    }
+    Product("0","1","0","1","1","0","1","0","1","1","1","1","0","1","0","1","1","0","1","0","1","1  ","   1","1") should matchPattern{
+      case Product(false,true,false,true,true,false,true,false,true,true,true,true,false,true,false,true,true,false,true,false,true,true,true,true) =>
+    }
+  }
+  it should "throw exception for invalid input" in {
+    an[Exception] should be thrownBy Product("23","1","0","1","1","0","1","0","1","1","1","1","0","1","0","1","1","0","1","0","1","1","1","1")
+  }
+
+  behavior of "Product.intToBool"
+  it should "work for valid input" in {
+    Product.intToBool(1) shouldBe true
+    Product.intToBool(0) shouldBe false
+  }
+  it should "throw exception for invalid input" in {
+    an[Exception] should be thrownBy Product.intToBool(2)
+  }
+
+}
