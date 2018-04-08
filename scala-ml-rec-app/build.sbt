8db5e9b2603261b506a667ba7da5fe2bbcad55eb
arpitHub <rawat.a@husky.neu.edu>
2018-04-08 16:06:13 -0400

    WIP #31 Added logger in data model for tracking progress

--- scala-ml-rec-app/build.sbt
+++ scala-ml-rec-app/build.sbt
@@ -4,7 +4,12 @@ version := "1.0"
 
 scalaVersion := "2.11.8"
 
-libraryDependencies ++= Seq("org.scalatest" %% "scalatest" % "3.0.1" % "test",
+libraryDependencies ++= Seq(
+  // logging
+  "org.apache.logging.log4j" % "log4j-api" % "2.4.1",
+  "org.apache.logging.log4j" % "log4j-core" % "2.4.1",
+
+  "org.scalatest" %% "scalatest" % "3.0.1" % "test",
   "org.apache.spark" % "spark-core_2.11" % "2.2.0",
   "org.apache.spark" % "spark-mllib_2.11" % "2.2.0"
 )
--- scala-ml-rec-app/src/main/scala/App.scala
+++ scala-ml-rec-app/src/main/scala/App.scala
@@ -5,37 +5,39 @@ import org.apache.spark.mllib.tree.model.RandomForestModel
 import org.apache.spark.mllib.util.MLUtils
 import org.apache.spark.{SparkConf, SparkContext}
 
-// Load and parse the data file.
-val conf = new SparkConf().setAppName("DataModel").setMaster("local")
-val sc = new SparkContext(conf)
-val data = MLUtils.loadLibSVMFile(sc, "data/cleaned_data.csv")
+object DataModelApp extends App {
+    // Load and parse the data file.
+    val conf = new SparkConf().setAppName("DataModel").setMaster("local")
+    val sc = new SparkContext(conf)
+    val data = MLUtils.loadLibSVMFile(sc, "data/cleaned_data.csv")
 
-// Split the data into training and test sets (30% held out for testing)
-val splits = data.randomSplit(Array(0.7, 0.3))
-val (trainingData, testData) = (splits(0), splits(1))
+    // Split the data into training and test sets (30% held out for testing)
+    val splits = data.randomSplit(Array(0.7, 0.3))
+    val (trainingData, testData) = (splits(0), splits(1))
 
-// Train a RandomForest model.
-// Empty categoricalFeaturesInfo indicates all features are continuous.
-val numClasses = 2
-val categoricalFeaturesInfo = Map[Int, Int]()
-val numTrees = 3 // Use more in practice.
-val featureSubsetStrategy = "auto" // Let the algorithm choose.
-val impurity = "gini"
-val maxDepth = 4
-val maxBins = 32
+    // Train a RandomForest model.
+    // Empty categoricalFeaturesInfo indicates all features are continuous.
+    val numClasses = 2
+    val categoricalFeaturesInfo = Map[Int, Int]()
+    val numTrees = 3 // Use more in practice.
+    val featureSubsetStrategy = "auto" // Let the algorithm choose.
+    val impurity = "gini"
+    val maxDepth = 4
+    val maxBins = 32
 
-val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
-numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
+    val model = RandomForest.trainClassifier(trainingData, numClasses, categoricalFeaturesInfo,
+      numTrees, featureSubsetStrategy, impurity, maxDepth, maxBins)
 
-// Evaluate model on test instances and compute test error
-val labelAndPreds = testData.map { point =>
-val prediction = model.predict(point.features)
-(point.label, prediction)
+    // Evaluate model on test instances and compute test error
+    val labelAndPreds = testData.map { point =>
+      val prediction = model.predict(point.features)
+      (point.label, prediction)
+    }
+    val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
+    println(s"Test Error = $testErr")
+    println(s"Learned classification forest model:\n ${model.toDebugString}")
+
+    // Save and load model
+    model.save(sc, "target/tmp/myRandomForestClassificationModel")
+    val sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestClassificationModel")
 }
-val testErr = labelAndPreds.filter(r => r._1 != r._2).count.toDouble / testData.count()
-println(s"Test Error = $testErr")
-println(s"Learned classification forest model:\n ${model.toDebugString}")
-
-// Save and load model
-model.save(sc, "target/tmp/myRandomForestClassificationModel")
-val sameModel = RandomForestModel.load(sc, "target/tmp/myRandomForestClassificationModel")
