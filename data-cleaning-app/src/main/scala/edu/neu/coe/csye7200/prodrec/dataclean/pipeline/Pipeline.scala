5fd2d16249cf27d388fc9f5a3772feadcc1de2ad
Nishant <nishantgandhi99@gmail.com>
2018-04-09 23:12:14 -0400

    WIP: #19 Data Cleaning and Preprocessing

--- data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/pipeline/Pipeline.scala
+++ data-cleaning-app/src/main/scala/edu/neu/coe/csye7200/prodrec/dataclean/pipeline/Pipeline.scala
@@ -3,6 +3,7 @@ package edu.neu.coe.csye7200.prodrec.dataclean.pipeline
 import edu.neu.coe.csye7200.prodrec.dataclean.io.DataParser
 import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
 import shapeless.{HList, ::, HNil}
+import org.apache.spark.sql.functions.col
 
 object Pipeline {
 
@@ -19,6 +20,24 @@ object Pipeline {
     //Step 3: Filter for customer who failed parsing Customer.code
     val classDS1 = classDS.filter(d => d.customerInfo.code != None)
 
+//    for(x <- df.columns)
+////      for(y <- df.select(col(x)).columns)
+////        println(y)
+//     df.select(col(x)).map( x=>x._1 ).show()
+
+    //TODO: Find better way to do this
+    val df = classDS1.toDF()
+    val newDf = df.withColumn("code",col("customerInfo.code"))
+      .withColumn("employmentStatus",col("customerInfo.employmentStatus"))
+      .withColumn("countryOfResidency",col("customerInfo.countryOfResidence"))
+      .withColumn("gender",col("customerInfo.gender"))
+      .withColumn("age",col("customerInfo.age"))
+      .withColumn("income",col("customerInfo.income"))
+      .drop("customerInfo")
+
+    newDf.show()
+
+
     //Step : Create DataFrame: Rename Columns and manually flatter from Dataset[SantanderRecord]
     val colNames = Seq("code","employmentStatus","countryOfResidency","gender","age","income",
       "customerType","joinDate","isCustomerAtMost6MonthOld","seniority","isPrimaryCustomer","customerTypeFirstMonth",
@@ -28,20 +47,20 @@ object Pipeline {
       "securities","homeAcc","payrollNom","pensionNom","directDebit"
     )
 
-    val df = classDS1.map(x => (x.customerInfo.code,x.customerInfo.employmentStatus,x.customerInfo.countryOfResidence,x.customerInfo.gender,x.customerInfo.age,x.customerInfo.income,
-      x.accountInfo.customerType,x.accountInfo.joinDate,x.accountInfo.isCustomerAtMost6MonthOld,x.accountInfo.seniority,x.accountInfo.isPrimaryCustomer,x.accountInfo.customerTypeFirstMonth,
-      x.accountInfo.customerRelationTypeFirstMonth,x.accountInfo.customerResidenceIndex,x.accountInfo.customerForeignIndex,x.accountInfo.channelOfJoin,x.accountInfo.deceasedIndex,x.accountInfo.customerAddrProvinceName,x.accountInfo.isCustomerActive,
-      x.productInfo.savingAcc,x.productInfo.guarantees,x.productInfo.currentAcc,x.productInfo.derivedAcc,x.productInfo.payrollAcc,x.productInfo.juniorAcc,x.productInfo.moreParticularAcc,x.productInfo.particularAcc,x.productInfo.particularPlusAcc,
-      x.productInfo.shortTermDeposit,x.productInfo.midTermDeposit,x.productInfo.longTermDeposit,x.productInfo.eAccount,x.productInfo.funds,x.productInfo.mortgage,x.productInfo.pensionPlan,x.productInfo.loan,x.productInfo.taxes,x.productInfo.creditCard,
-      x.productInfo.securities,x.productInfo.homeAcc,x.productInfo.payrollNom,x.productInfo.pensionNom,x.productInfo.directDebit
-    )).toDF(colNames: _*)
-
-//    df.show()
+//    val df = classDS1.map(x => (x.customerInfo.code,x.customerInfo.employmentStatus,x.customerInfo.countryOfResidence,x.customerInfo.gender,x.customerInfo.age,x.customerInfo.income,
+//      x.accountInfo.customerType,x.accountInfo.joinDate,x.accountInfo.isCustomerAtMost6MonthOld,x.accountInfo.seniority,x.accountInfo.isPrimaryCustomer,x.accountInfo.customerTypeFirstMonth,
+//      x.accountInfo.customerRelationTypeFirstMonth,x.accountInfo.customerResidenceIndex,x.accountInfo.customerForeignIndex,x.accountInfo.channelOfJoin,x.accountInfo.deceasedIndex,x.accountInfo.customerAddrProvinceName,x.accountInfo.isCustomerActive,
+//      x.productInfo.savingAcc,x.productInfo.guarantees,x.productInfo.currentAcc,x.productInfo.derivedAcc,x.productInfo.payrollAcc,x.productInfo.juniorAcc,x.productInfo.moreParticularAcc,x.productInfo.particularAcc,x.productInfo.particularPlusAcc,
+//      x.productInfo.shortTermDeposit,x.productInfo.midTermDeposit,x.productInfo.longTermDeposit,x.productInfo.eAccount,x.productInfo.funds,x.productInfo.mortgage,x.productInfo.pensionPlan,x.productInfo.loan,x.productInfo.taxes,x.productInfo.creditCard,
+//      x.productInfo.securities,x.productInfo.homeAcc,x.productInfo.payrollNom,x.productInfo.pensionNom,x.productInfo.directDebit
+//    )).toDF(colNames: _*)
 //
-//    val df = classDS1
-//      .flatMap()
-//    val df = classDS1.map( x => x.customerInfo.income :: x.customerInfo.gender)
-    df.show()
+////    df.show()
+////
+////    val df = classDS1
+////      .flatMap()
+////    val df = classDS1.map( x => x.customerInfo.income :: x.customerInfo.gender)
+//    df.show()
 
     classDS1.toDF()
   }
